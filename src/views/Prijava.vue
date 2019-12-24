28bdd857c7003fd1831154f784ca03df8bc3a23e
starcictoni <toni.starcic@outlook.com>
2019-12-24 12:43:05 +0100

    Ovisnosti

--- src/views/Prijava.vue
+++ src/views/Prijava.vue
@@ -1,134 +1,49 @@
 <template>
-    <div class="app">
-        Prijava
-        <div class="text-center">
-	<!-- Button HTML (to Trigger Modal) -->
-	<a href="#myModal" class="trigger-btn" data-toggle="modal">Click to Open Login Modal</a>
-</div>
+  <div class="about">
+    <h1>This is a login page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+      <div class="row">
+        <div class="col"></div>
 
-<!-- Modal HTML -->
-<div id="myModal" class="modal fade">
-	<div class="modal-dialog modal-login">
-		<div class="modal-content">
-			<div class="modal-header">
-				<div class="avatar">
-					<img src="/examples/images/avatar.png" alt="Avatar">
-				</div>				
-				<h4 class="modal-title">Member Login</h4>	
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-			</div>
-			<div class="modal-body">
-				<form action="/examples/actions/confirmation.php" method="post">
-					<div class="form-group">
-						<input type="text" class="form-control" name="username" placeholder="Username" required="required">		
-					</div>
-					<div class="form-group">
-						<input type="password" class="form-control" name="password" placeholder="Password" required="required">	
-					</div>        
-					<div class="form-group">
-						<button type="submit" class="btn btn-primary btn-lg btn-block login-btn">Login</button>
-					</div>
-				</form>
-			</div>
-			<div class="modal-footer">
-				<a href="#">Forgot Password?</a>
-			</div>
-		</div>
-	</div>
-</div>     
+        <div class="col">
+          <form @submit.prevent="login">
+            <div class="form-group">
+              <label for="emailField">Email address</label>
+              <input v-model="email" type="email" class="form-control" id="emailField" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+              <label for="passwordField">Password</label>
+              <input v-model="password" type="password" class="form-control" id="passwordField" placeholder="Password"/>
+            </div>
+            <button type="submit" class="btn btn-primary mt-5">Submit</button>
+          </form>
+          
+        </div>
+        <div class="col"></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
-
-</script>
-
-<style lang="scss">
-    body {
-		font-family: 'Varela Round', sans-serif;
-	}
-	.modal-login {		
-		color: #636363;
-		width: 350px;
-	}
-	.modal-login .modal-content {
-		padding: 20px;
-		border-radius: 5px;
-		border: none;
-	}
-	.modal-login .modal-header {
-		border-bottom: none;   
-        position: relative;
-        justify-content: center;
-	}
-	.modal-login h4 {
-		text-align: center;
-		font-size: 26px;
-		margin: 30px 0 -15px;
-	}
-	.modal-login .form-control:focus {
-		border-color: #70c5c0;
-	}
-	.modal-login .form-control, .modal-login .btn {
-		min-height: 40px;
-		border-radius: 3px; 
-	}
-	.modal-login .close {
-        position: absolute;
-		top: -5px;
-		right: -5px;
-	}	
-	.modal-login .modal-footer {
-		background: #ecf0f1;
-		border-color: #dee4e7;
-		text-align: center;
-        justify-content: center;
-		margin: 0 -20px -20px;
-		border-radius: 5px;
-		font-size: 13px;
-	}
-	.modal-login .modal-footer a {
-		color: #999;
-	}		
-	.modal-login .avatar {
-		position: absolute;
-		margin: 0 auto;
-		left: 0;
-		right: 0;
-		top: -70px;
-		width: 95px;
-		height: 95px;
-		border-radius: 50%;
-		z-index: 9;
-		background: #60c7c1;
-		padding: 15px;
-		box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
-	}
-	.modal-login .avatar img {
-		width: 100%;
-	}
-	.modal-login.modal-dialog {
-		margin-top: 80px;
-	}
-    .modal-login .btn {
-        color: #fff;
-        border-radius: 4px;
-		background: #60c7c1;
-		text-decoration: none;
-		transition: all 0.4s;
-        line-height: normal;
-        border: none;
+    data () {
+    return {
+      email: '',
+      password: ''
     }
-	.modal-login .btn:hover, .modal-login .btn:focus {
-		background: #45aba6;
-		outline: none;
-	}
-	.trigger-btn {
-		display: inline-block;
-		margin: 100px auto;
-	}
-</style>
-
+  },
+  methods: {
+    login () {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
--- src/views/Registracija.vue
+++ src/views/Registracija.vue
@@ -1,15 +1,76 @@
 <template>
-    <div class="app">
-        Registracija
+  <div class="about">
+    <h1>This is a signup page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+      <div class="row">
+        <div class="col"></div>
+        <div class="col">
+
+          <form @submit.prevent="signup"><!--Nemoj poslati na server-->
+            <div class="form-group">
+              <!--Mail-->
+              <label for="emailField">Email address</label>
+              <input v-model="email" type="email" class="form-control" id="emailField" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            
+            <!--Password-->
+            <div class="form-group">
+              <label for="passwordField">Password</label>
+              <input v-model="password" type="password" class="form-control" id="passwordField" placeholder="Password"/>
+            </div>
+
+            <!--Password 2-->
+            <div class="form-group">
+              <label for="confirmPasswordField">Confirm Password</label>
+              <input v-model="password2" type="password" class="form-control" id="confirmPasswordField" placeholder="Confirm password"/>
+            </div>
+
+            <div v-if="password != password2" class="alert alert-danger" role="alert">Passwords do not match.</div> <!--Promijeni css-->
+            <button v-if="password == password2" type="submit" class="btn btn-primary mt-5">Submit</button>
+          </form>
+
+        </div>
+        <div class="col"></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
+  name: "signup",
+  data () {
+    return {
+      email: "",
+      password: "",
+      password2: ""
+    }
+  },
+  methods: {
+    signup () {
+      debugger;
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).catch(function(error) {
+        console.log(error);
+      });
+    },
+  }
 }
 </script>
 
 <style lang="scss">
-
+form {
+  text-align: left;
+  label {
+    margin-top: 10px;
+    margin-left: 15px;
+  }
+  ::placeholder {
+    color: gray;
+  }
+}
 </style>
