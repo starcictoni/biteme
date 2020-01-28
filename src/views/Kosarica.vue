8f007cd30b08f449ccd24ab95302897ad3a7bf9c
Sandi <blekic.sandi@gmail.com>
2020-01-28 21:30:02 +0100

    checkout stranica

--- src/views/Kosarica.vue
+++ src/views/Kosarica.vue
@@ -1,18 +1,35 @@
 <template>
-    <v-container>
-        <!-- ovo je testirnje sb -->
-        <p>{{kupovina[0]}}</p>   
-         <p>{{kupovina[1]}}</p>  
-    </v-container>
+  <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Namirnica</th>
+            <th class="text-left">Cijena</th>
+            <th class="text-left">Stanje</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in kupovina" :key="item">
+                <td>{{ item[0] }}</td>
+                <td>{{ item[1] + " kn" }}</td>
+                <td>{{ item[2] }}</td>
+
+            </tr>
+      
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>
 import localStore from "@/localStore.js";
 
 export default {
-    name:"kosarica",    
-    data() {
-        return localStore;
-    }
+  name: "kosarica",
+  data() {
+    return localStore;
+  }
 };
 </script>
